56ded2b9efab490b8e3e881cde8aee37a285fe4f
Britt Carter <brittbmh@gmail.com>
2019-06-27 14:19:27 -0500

    add new project to demo Lists

---
+++ cscountriesList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cscountriesList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Projects\tutorials\cscountriesList\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_100_000);
+            int lilliputIndex = countries.FindIndex(x=>x.Population < 2_100_000);
+            countries.Insert(lilliputIndex, lilliput);
+            countries.RemoveAt(lilliputIndex);
+            
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            Console.WriteLine($"{countries.Count} countries");
+        }
+    }
+}
