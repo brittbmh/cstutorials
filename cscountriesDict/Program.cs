7f850becead9c807a22d2e26d36becd12fd678a4
Britt Carter <brittbmh@gmail.com>
2019-06-27 15:42:17 -0500

    add dictionary tutorial

---
+++ cscountriesDict/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cscountriesDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Projects\tutorials\cscountriesList\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
+            
+            Console.WriteLine("which country code do you want to look up?");
+            string userInput = Console.ReadLine();
+            
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if (!gotCountry)
+                Console.WriteLine($"Sorry, there is no country with code {userInput}");
+            else
+                Console.WriteLine($"{country.Name} has population {PopulationFormatter.FormatPopulation(country.Population)}");
+        }
+    }
+}
