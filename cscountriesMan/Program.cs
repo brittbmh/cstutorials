9e5ad9cc98fed0c92e971e8c4a8491e2bff21ee0
Britt Carter <brittbmh@gmail.com>
2019-06-27 16:29:03 -0500

    add project to manipulate data

---
+++ cscountriesMan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cscountriesMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Projects\tutorials\cscountriesList\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+            
+            for (int i=0; i < countries.Count; i++)
+            {
+                Country country = countries[i];
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
