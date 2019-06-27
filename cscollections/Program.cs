705d555bc30a9fae84d52cc223332b813642a2e0
Britt Carter <brittbmh@gmail.com>
2019-06-27 09:20:52 -0500

    first commit

---
+++ cscollections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cscollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize array
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
